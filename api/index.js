936ab379846c9335df864757319b78487d223404
Leonardo Gama <leonardogama2000@outlook.com.br>
2025-04-19 18:00:39 -0400

    feat: refactor API handler to include robots.txt response and improve SSR integration

--- api/index.js
+++ api/index.js
@@ -1,3 +1,11 @@
-export default import("../dist/popfragg/server/server.mjs").then((module) =>
-  module.app()
-);
+export default async function handler(req, res) {
+  if (req.url === "/robots.txt") {
+    const isProd = process.env.VERCEL_ENV === "production";
+    res.setHeader("Content-Type", "text/plain");
+    res.end("User-agent: *\nDisallow: /");
+    return;
+  }
+
+  const { app } = await import("../dist/popfragg/server/server.mjs");
+  return app()(req, res); // repassa a requisição pro app Angular SSR
+}
